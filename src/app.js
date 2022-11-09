dbc325d065c42e5818f61f3c2446978a432e00ef
Steven Vicino <stevenvicino@gmail.com>
2022-11-09 12:47:42 -0500

    added cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
